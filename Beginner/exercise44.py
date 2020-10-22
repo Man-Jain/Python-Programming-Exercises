7fe109e120479cfe78d5624eca02db9d6cd6f2ba
Aditya Vyas <adivyas99@gmail.com>
2020-10-22 21:38:33 +0530

    Update exercise44.py

--- Beginner/exercise44.py
+++ Beginner/exercise44.py
@@ -1,3 +1,11 @@
+#Pythonic way
 l=map(lambda x:x**2, range(1,21))
 
-print(list(l))
+#layman methord 
+def av(a,b):
+  l=[]
+  for i in range(a,b):
+    x=i**2
+    l.append(x)
+   print(l)
+av(12,30)
